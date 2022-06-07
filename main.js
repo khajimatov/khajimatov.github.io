e1810f3377f012c6d8aaa32ff632b0f99ac0c073
Bakha <for.official.bkh@gmail.com>
2022-06-07 15:06:13 +0600

    Added Javascipt for social links + tooltips

---
+++ main.js
@@ -0,0 +1,9 @@
+const Insta = () => {
+  window.open('https://instagram.com/khajimatovv', '_blank')
+}
+const Linkedin = () => {
+  window.open('https://linkedin.com/in/khajimatov', '_blank')
+}
+const Github = () => {
+  window.open('https://github.com/khajimatov', '_blank')
+}
